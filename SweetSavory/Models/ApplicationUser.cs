4f154d0091e71c12a274adaca0f826a47245a9f6
Ben Wilson <benjaminw1030@gmail.com>
2021-10-22 11:01:38 -0700

    add account views and homepage

--- SweetSavory/Models/ApplicationUser.cs
+++ SweetSavory/Models/ApplicationUser.cs
@@ -1,9 +1,10 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace SweetSavory.Models
 {
   public class ApplicationUser : IdentityUser
   {
-    
+
   }
 }
